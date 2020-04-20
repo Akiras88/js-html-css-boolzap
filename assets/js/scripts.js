c98168260d8dd53128b052c02f7c9bffb927c171
Francesco <francesco.scarnera3@gmail.com>
2020-04-20 16:15:50 +0200

    fix send message user

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,42 +1,88 @@
+
 $(document).ready(function(){
     // reference
     var newInput = $('.input-write input');
-    var main = $('.Main');
-    var microphone = $('.fa-microphone');
-    var plane = $('.fa-paper-plane');
+    var sendIcon = $('.icon-footer .fa-microphone');
 
 
     // change icon to send chat
 
-    $('footer').on('mouseenter', '.input-write', function(){
-        microphone.toggleClass('hide');
-        plane.toggleClass('hide');
+    newInput.on('focus blur', function() {
+        sendIcon.toggleClass('fa-microphone fa-paper-plane');
     });
-    $('footer').on('mouseleave', '.input-write', function(){
-        microphone.toggleClass('hide');
-        plane.toggleClass('hide');
-    });
+    
+
+
+
 
 
     // add a dynamic template 
 
-    newInput.keyup(function(event){
-        if (event.which == 13 || event.keycode == 13) { // 13 is enter keycode
-            var text = newInput.val().trim();
+    // with icon
+    sendIcon.click(function() {
+        sendMessage(newInput);
+    });
 
-            // validation
-            if (text !== ' ') {
-                var chatNew = $('.template .user-chat .cloud').clone().prepend(text);
-                main.append(chatNew);
-                // clear input
-                newInput.val('');
-            }
+    // with enter
+    newInput.keypress(function(e) {
+        if(e.which == 13) {
+            sendMessage(newInput);
         }
     });
 
 
+}); // <-- End doc ready
+
+
+
+/****************************************************************
+    FUNCTIONS
+ ****************************************************************/
+
+// send new message function
+
+function sendMessage(input) {
+    
+    // user text
+    var textUser = input.val().trim();
+
+    // check
+    if ( (textUser.length > 0) && (textUser != ' ' )) {
+
+        // clone template
+        var newMessage = $('.template .chat-green').clone();
+
+        // add text message to template
+        newMessage.children('.cloud').text(textUser);
+
+        // add current time
+        var date = new Date();
+        var hour = addZero( date.getHours() );
+        var minutes = addZero( date.getMinutes() );
+        var time = hour + ':' + minutes;
+        newMessage.children('.time').text(time);
+
+        newMessage.addClass('.user-chat');
+
+        $('.Main.active').append(newMessage);
+
+        // reset input 
+        input.val('');
+
+
+    }
 
 
 
 
-}); // end jQuery
+
+}
+
+// Add leading zero to numbers less than 10 whith function
+function addZero(numero) {
+    if(numero < 10) {
+        numero = '0' + numero;
+    }
+    
+    return numero;
+}
